afface4ffdef3598e27fd3592570894d35b84bfc
Joy <380196088@qq.com>
2020-01-16 17:02:53 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import { render } from 'react-dom';
+import PropType from 'prop-types';//自定义组件属性类型校验
+import {
+  Button, Input
+} from 'antd';
+
+import Header from './components/Header/index'
+
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'antd/dist/antd.css'
 import './App.css';
+
+moment.locale('zh-cn');
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/components/LeftNav/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Row } from 'antd';
+export default class Admin extends React.Component{
+  render(){
+    return (
+      <Row>
+        <Col span='3'></Col>
+        <Col span='21'></Col>
+      </Row>
+    )
+  }
+}
